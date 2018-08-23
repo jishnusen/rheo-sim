ecd1cb7f8cf3dcb459f083c46efe7a97fd082173
Jishnu Sen <jishnu1@gmail.com>
2018-08-23 09:50:35 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Shear Modulus Spectra Calculator
+
+Implements [this paper](https://arxiv.org/abs/1711.09669)
+
+## Setup
+Download and extract [https://github.com/jishnusen/rheo-sim/zipball/master](https://github.com/jishnusen/rheo-sim/zipball/master)
+
+### Windows
+It is recommended to use the Anaconda distribution of python for windows, as it
+installs many modules by default. Go to [https://www.anaconda.com/download/](https://www.anaconda.com/download) to download anaconda.
+
+Once anaconda is installed, open the program Anaconda Prompt, and type:
+```
+conda install -c conda-forge lmfit 
+```
+
+### Linux/MacOS
+Most major distributions of Linux come preinstalled with python3.
+
+On Mac, install with
+```
+brew install python
+```
+If the above command does not work, install Homebrew with
+```
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+```
+
+Open your terminal emulator (Usually Terminal.app on Mac), and type
+```
+python3 -m pip install -r requirements.txt
+```
+If on Linux, ensure you have TKinter installed (this can be done with `sudo apt install python3-tk` on ubuntu)
+
+## Usage
+Run the script with
+```
+python3 rheo.py
+```
+You can make the script save its results to a csv outfile by specifying the file
+name as an argument:
+```
+python3 rheo.py outfile.csv
+```
+The program will open a file chooser window when it is run. Here, choose a CSV
+with all the experimental data. It expects the CSV to have columns: AF, SM, and
+LM, for Angular Frequency (rad/s), Storage Modulus (GPa), and Loss Modulus (GPa), respectively.
---
+++ rheo.py
@@ -0,0 +1,88 @@
+import scipy.integrate as integrate
+import numpy as np
+import cmath
+import pandas as pd
+from lmfit import Model
+import sys
+from tkinter import filedialog
+import tkinter
+
+tkinter.Tk().withdraw()
+data = pd.read_csv(filedialog.askopenfilename())
+
+
+def l_r(v, beta, f):
+    return (f * (8 - (4 * beta)) / 3) * cmath.exp(beta * v) * cmath.exp(-cmath.exp(v))
+
+
+def integrand(x, omega, tau_c, beta, f):
+    return l_r(x, beta, f) / (1 + 1j * omega * (tau_c * cmath.exp(x)))
+
+
+def re_int(x, omega, tau_c, beta, f):
+    try:
+        return np.real(integrand(x, omega, tau_c, beta, f))
+    except OverflowError:
+        return 0
+
+
+def im_int(x, omega, tau_c, beta, f):
+    try:
+        return np.imag(integrand(x, omega, tau_c, beta, f))
+    except OverflowError:
+        return 0
+
+
+#eta = float(input("Enter eta: "))
+kappa = 8.
+def function(x, G_inf, beta, f, eta):
+    tau_m = eta / G_inf
+    tau_c = kappa * tau_m
+
+    re = np.zeros(x.size)
+    im = np.zeros(x.size)
+    for i in range(0, x.size):
+        re[i] = (integrate.quad(
+            re_int, -np.inf, +np.inf, args=(x[i], tau_c, beta, f)))[0]
+        im[i] = (integrate.quad(
+            im_int, -np.inf, +np.inf, args=(x[i], tau_c, beta, f)))[0]
+
+    integral = re + (im * 1j)
+    tmp = 1 + integral - (1j / (x * tau_m))
+    G_res = G_inf / tmp
+    return G_res
+
+
+X = np.array(data.AF)
+YReal = np.array(data.SM/ 1e9)
+YImag = np.array(data.LM/1e9)
+Y = YReal + YImag * 1j
+
+from matplotlib import pyplot as plt
+
+model = Model(function)
+params = model.make_params()
+params['G_inf'].set(value=0.8, min=0.1, max=30)
+params['beta'].set(value=0.6, min=0.1, max=1)
+#params['kappa'].set(value=8, min=1, max=50)
+params['eta'].set(value=0.1, min=0, max=100)
+params['f'].set(value=0.5, min=0.1, max=2)
+print("Fitting...")
+result = model.fit(Y, x=X, params=params) 
+YFit = result.best_fit
+print("Complete!", result.best_values)
+plt.scatter(X, np.real(Y))
+plt.scatter(X, np.imag(Y))
+plt.plot(X, np.real(YFit))
+plt.plot(X, np.imag(YFit))
+plt.yscale('log')
+plt.xscale('log')
+plt.show()
+
+if len(sys.argv) > 1:
+    out = open(str(sys.argv[1]), "w+")
+    csv = "Omega,G_Prime,G_Double_Prime\n"
+    for i in range(0, X.size):
+        csv += f"{float(X[i])},{float(np.real(YFit[i]))},{float(np.imag(YFit[i]))}"
+        csv += "\n"
+    out.write(csv)
